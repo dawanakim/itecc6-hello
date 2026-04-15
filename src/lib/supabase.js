95c339e6144217e352ee4883282a988154749d39
dawanakim <agcaoiledawana@email.com>
2026-04-15 11:29:32 +0800

    feat: initial Vue.js Hello World setup

---
+++ src/lib/supabase.js
@@ -0,0 +1,4 @@
+import { createClient } from '@supabase/supabase-js' 
+const supabaseUrl = 'https://cmjgbtatiexlmuvqiolb.supabase.co' 
+const supabaseKey = 'sb_publishable_95v_6-GT_YGSA8wqFBoivw_a2U9BXHp' 
+export const supabase = createClient(supabaseUrl, supabaseKey
